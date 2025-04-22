a6a179fdbeb9c404e47318cd51d33bb2350959be
Parker Freestone <parkerfreestone@gmail.com>
2025-04-21 22:07:15 -0600

    :sparkles: Fixed repo, added everything to one Enum file.

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public enum CompanyEnums
+{
+    Anisense,
+    Monger,
+    Parlax,
+    None
+}
+
+public enum RobotEnums
+{
+    Mining,
+    Combat,
+    Basic,
+    Main,
+    None
+}
+public enum ResourceType
+{
+    Iron,
+    Copper,
+    HydrogenIce,
+    Silicon,
+    Titanium,
+    Tungsten,
+    Brainium
+}
